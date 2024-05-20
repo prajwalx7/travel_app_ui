d6676d25daa6f2a9ad1c93daa2e0edc40a13de57
prajwal77d <prajwal77d@gmail.com>
2024-05-20 16:41:46 +0000

    list

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,9 +1,6 @@
 import 'dart:ui';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -13,187 +10,220 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xffEBECF9),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Container(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(28)),
+                        child: const Icon(Icons.sort)),
+                    const Spacer(),
+                    Container(
                       padding: const EdgeInsets.all(8),
                       decoration: BoxDecoration(
                           color: Colors.white,
                           borderRadius: BorderRadius.circular(28)),
-                      child: const Icon(Icons.sort)),
-                  const Spacer(),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(28)),
-                    child: const Icon(Icons.search),
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 14,
-              ),
-              const Row(
-                children: [
-                  Text(
-                    "Find the trip \nof your dreams",
-                    style: TextStyle(fontSize: 22),
-                  ),
-                  Spacer(),
-                  Icon(Icons.settings)
-                ],
-              ),
-              SizedBox(
-                height: 40,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  shrinkWrap: true,
-                  children: [
-                    "America",
-                    "Japan",
-                    "Paris",
-                    "France",
-                    "America",
-                    "Japan",
-                    "Paris",
-                    "France",
-                    "America",
-                    "Japan",
-                    "Paris",
-                    "France"
-                  ]
-                      .map((city) => Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 8),
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 12, vertical: 8),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                          child: Text(
-                            city,
-                            style: const TextStyle(fontWeight: FontWeight.w500),
-                          )))
-                      .toList(),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: double.maxFinite,
-                height: 300,
-                decoration: BoxDecoration(
-                  image: const DecorationImage(
-                      image: AssetImage("assets/sing.jpg"), fit: BoxFit.cover),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Column(
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                      child: Row(
-                        children: [
-                          BlurWidget(
-                            child: Text(
-                              "\$400/Person",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ),
-                          Spacer(),
-                          BlurWidget(
-                            child: Icon(
-                              Icons.favorite,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const Spacer(),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20, bottom: 10),
-                      child: SizedBox(
-                        width: 500,
-                        child: Row(
-                          children: [
-                            const Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  "Isola Bella",
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 28),
-                                ),
-                                Row(
-                                  children: [
-                                    Icon(
-                                      Icons.location_on,
-                                      color: Colors.white,
-                                    ),
-                                    Text(
-                                      "Singapore",
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                BlurWidget(
-                                  child: Row(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Icon(Icons.star, color: Colors.yellow),
-                                      Text(
-                                        "4.5",
-                                        style: TextStyle(color: Colors.white),
-                                      ),
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                            const Spacer(),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.only(top: 50, right: 10),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(50),
-                                ),
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 16, vertical: 8),
-                                child: const Row(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Text(
-                                      "Explore",
-                                      style: TextStyle(color: Colors.black),
-                                    ),
-                                    SizedBox(width: 8),
-                                    Icon(
-                                      Icons.arrow_circle_right,
-                                      color: Colors.black,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
+                      child: const Icon(Icons.search),
                     ),
                   ],
                 ),
-              ),
-            ],
+                const SizedBox(
+                  height: 14,
+                ),
+                const Row(
+                  children: [
+                    Text(
+                      "Find the trip \nof your dreams",
+                      style: TextStyle(fontSize: 22),
+                    ),
+                    Spacer(),
+                    Icon(Icons.settings)
+                  ],
+                ),
+                SizedBox(
+                  height: 40,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    children: [
+                      "America",
+                      "Japan",
+                      "Paris",
+                      "France",
+                      "America",
+                      "Japan",
+                      "Paris",
+                      "France",
+                      "America",
+                      "Japan",
+                      "Paris",
+                      "France"
+                    ]
+                        .map((city) => Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 8),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 12, vertical: 8),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            child: Text(
+                              city,
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.w500),
+                            )))
+                        .toList(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.vertical,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: 3,
+                  itemBuilder: (context, index) => Container(
+                    width: double.maxFinite,
+                    padding: const EdgeInsets.symmetric(vertical: 12),
+                    margin: const EdgeInsets.only(top: 16),
+                    height: 300,
+                    decoration: BoxDecoration(
+                      image: const DecorationImage(
+                          image: AssetImage("assets/photo3.jpg"),
+                          fit: BoxFit.cover),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      children: [
+                        const Padding(
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                          child: Row(
+                            children: [
+                              BlurWidget(
+                                child: Text(
+                                  "\$400/Person",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                              Spacer(),
+                              BlurWidget(
+                                child: Icon(
+                                  Icons.favorite,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const Spacer(),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20, bottom: 10),
+                          child: SizedBox(
+                            width: 500,
+                            child: Row(
+                              children: [
+                                const Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      "Mount Fuji",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 28,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(
+                                      height: 5,
+                                    ),
+                                    Row(
+                                      children: [
+                                        Icon(
+                                          Icons.location_on,
+                                          color: Colors.white,
+                                        ),
+                                        Text(
+                                          "Japan",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 16),
+                                        ),
+                                      ],
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    BlurWidget(
+                                      child: Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Icon(Icons.star,
+                                              color: Colors.yellow),
+                                          Text(
+                                            "4.5",
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                const Spacer(),
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.only(top: 70, right: 10),
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  const HomeScreen()));
+                                    },
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        borderRadius: BorderRadius.circular(50),
+                                      ),
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 16, vertical: 8),
+                                      child: const Row(
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Text(
+                                            "Explore",
+                                            style:
+                                                TextStyle(color: Colors.black),
+                                          ),
+                                          SizedBox(width: 8),
+                                          Icon(
+                                            Icons.arrow_right_alt,
+                                            color: Colors.black,
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -215,7 +245,7 @@ class BlurWidget extends StatelessWidget {
       child: BackdropFilter(
         filter: ImageFilter.blur(sigmaY: 20, sigmaX: 20),
         child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 5),
             decoration: const BoxDecoration(
               color: Colors.black12,
             ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/home_screen.dart';
 
 void main() {
@@ -11,10 +12,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-   debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-      
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme().copyWith(),
+      ),
+      home: const HomeScreen(),
     );
   }
 }
