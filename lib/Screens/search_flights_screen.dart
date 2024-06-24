545ddbc8a8ce1f88f486748e39726ad61350b539
prajwal <prajwal77d@gmail.com>
2024-06-24 14:17:01 +0530

    flights

--- lib/Screens/search_flights_screen.dart
+++ lib/Screens/search_flights_screen.dart
@@ -5,22 +5,24 @@ class SearchFlightsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xffEBECF9),
       appBar: AppBar(
-        title: const Text('Available Flights'),
+        backgroundColor: Colors.transparent,
+        title: const Text(
+          'Available Flights',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(vertical: 25.0, horizontal: 22.0),
         child: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FlightContainer(),
-                FlightContainer(),
-                FlightContainer(),
-              ],
-            ),
+            FlightContainer(),
+            SizedBox(height: 25),
+            FlightContainer(),
+            SizedBox(height: 25),
+            FlightContainer(),
           ],
         ),
       ),
@@ -29,13 +31,15 @@ class SearchFlightsScreen extends StatelessWidget {
 }
 
 class FlightContainer extends StatelessWidget {
+  const FlightContainer({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 100,
+      width: 400,
       height: 150,
       decoration: BoxDecoration(
-        color: Colors.blueAccent,
+        color: Colors.white,
         borderRadius: BorderRadius.circular(8),
       ),
       child: Column(
